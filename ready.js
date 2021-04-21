5e79f4facecf4882944e1624dcfbff317c118671
Muhammet Emin <66025604+MuhammetDev@users.noreply.github.com>
2021-04-21 14:32:58 +0300

    Add files via upload

---
+++ ready.js
@@ -0,0 +1,30 @@
+
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(
+    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: Aktif, Komutlar yüklendi!`
+  );
+  console.log(
+    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${
+      client.user.username
+    } ismi ile giriş yapıldı!`
+  );
+  client.user.setStatus("online");
+  var oyun = [
+    `f!yardım | ${client.guilds.cache.size} Sunucu`
+
+  ];
+//PLAYING Oynuyor //WATCHING İzliyor
+  setInterval(function() {
+    var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
+
+    client.user.setActivity(oyun[random], {type: 'PLAYING'});
+  }, 2 * 2500);
+};
+
